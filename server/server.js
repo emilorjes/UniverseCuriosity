55518de6311a11c3fbc2d62bcb98ef5f36d8eb9e
Emil Örjes <emil.orjes@hotmail.com>
2022-01-06 11:05:32 +0100

    Add bodyParser and cors to server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const planetRoutes = require('./routes/PlanetRoutes');
 
 const app = express();
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
 mongoose.connect(process.env.TEST_DB);
 app.listen(process.env.PORT);
 app.use('/planets', planetRoutes);
