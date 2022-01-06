8a090aaa5ae9029d97107b2285cb5177c1db9e1d
Emil Örjes <emil.orjes@hotmail.com>
2022-01-06 08:50:20 +0100

    Add planet route and see if it works in server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
-
 const express = require('express');
+const mongoose = require('mongoose');
+const planetRoutes = require('./routes/PlanetRoutes');
 
 const app = express();
-const mongoose = require('mongoose');
-
 mongoose.connect(process.env.TEST_DB);
 app.listen(process.env.PORT);
+app.use('/planets', planetRoutes);
