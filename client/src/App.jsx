668a5a5a6c584cc02924c236bb1f07ed6f6f88f2
Emil Örjes <emil.orjes@hotmail.com>
2022-01-12 15:01:02 +0100

    Add background gradient in Global.css and import Global.css to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Router from './routes/Router';
 import Navigations from './components/navigations/Navigations';
+import './shared/global/Global.css';
 
 function App() {
   return (
