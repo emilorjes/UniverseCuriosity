af8d58037cc1f2fc3a741159b3d02d52e462227a
Emil Örjes <74154767+emilorjes@users.noreply.github.com>
2022-01-14 09:37:11 +0100

    Footer (#14)
    * Add Footer folder/ jsx /css
    
    * Add getCurrent function and fix css and import to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Router from './routes/Router';
 import Navigations from './components/navigations/Navigations';
+import Footer from './components/footer/Footer';
 import './shared/global/Global.css';
 
 function App() {
   return (
     <Router>
       <Navigations />
+      <Footer />
     </Router>
   );
 }
