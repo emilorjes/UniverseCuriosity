d7c9c7789699880f49819c4b1929ea0922f33548
Emil Örjes <emil.orjes@hotmail.com>
2022-01-19 11:44:05 +0100

    Change endpoint in PlanetsAPIService.

--- client/src/shared/api/service/PlanetsAPIService.js
+++ client/src/shared/api/service/PlanetsAPIService.js
@@ -4,6 +4,6 @@ import http from '../PlanetsAPI';
  * Gets all planets with the help of /get endpoint from server side.
  * @returns Endpoint of get all planets.
  */
-const getAllPlanets = () => http.get('/get');
+const getAllPlanets = () => http.get('/getPlanets');
 
 export default { getAllPlanets };
