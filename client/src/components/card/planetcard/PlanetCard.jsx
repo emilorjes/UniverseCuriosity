266b68b7c0ef2fe6e5992d3f029c93c7e621c6d6
Emil Örjes <74154767+emilorjes@users.noreply.github.com>
2022-01-20 17:48:30 +0100

    Popup (#22)
    * Add function for planetModal
    
    * Add css and data to planetMoadal, change condition in PlanetCard

--- client/src/components/card/planetcard/PlanetCard.jsx
+++ client/src/components/card/planetcard/PlanetCard.jsx
@@ -1,19 +1,38 @@
-import { React, useContext } from 'react';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { React, useContext, useState } from 'react';
 import { PlanetsContext } from '../../../shared/provider/PlanetsProvider';
+import PlanetModal from '../../modal/planetmodal/PlanetModal';
 import './PlanetCard.css';
 
 function PlanetCard() {
   const { listOfPlanets } = useContext(PlanetsContext);
+  const [showModal, setShowModal] = useState(false);
+  const [modalData, setModalData] = useState(null);
+
+  const handleClick = (planet) => {
+    setModalData(planet);
+    setShowModal(true);
+  };
 
   return (
-    <div className="cards">
-      {listOfPlanets.map((planet) => (
-        <div className="card" key={planet.name}>
-          <h2> {planet.name}</h2>
-          <img src={planet.image} alt="" />
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="cards">
+        {listOfPlanets.map((planet) => (
+          <div
+            onClick={() => handleClick(planet)}
+            className="card"
+            key={planet.name}
+          >
+            <h2> {planet.name}</h2>
+            <img src={planet.image} alt="" />
+          </div>
+        ))}
+      </div>
+      {showModal && (
+        <PlanetModal planet={modalData} closeModal={setShowModal} />
+      )}
+    </>
   );
 }
 
