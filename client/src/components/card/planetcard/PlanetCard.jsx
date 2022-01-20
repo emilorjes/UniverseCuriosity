dc3b067cb16e3e76d6d0e3efc28cf948c6a34427
Emil Örjes <74154767+emilorjes@users.noreply.github.com>
2022-01-20 11:04:28 +0100

    Card (#20)
    * Add planet card
    
    * Add planet objects to planetModel, change route name.
    
    * Change endpoint in PlanetsAPIService.
    
    * Add css for planetCard, hoover, border
    
    * Add shadow to card and change border and transition

---
+++ client/src/components/card/planetcard/PlanetCard.jsx
@@ -0,0 +1,20 @@
+import { React, useContext } from 'react';
+import { PlanetsContext } from '../../../shared/provider/PlanetsProvider';
+import './PlanetCard.css';
+
+function PlanetCard() {
+  const { listOfPlanets } = useContext(PlanetsContext);
+
+  return (
+    <div className="cards">
+      {listOfPlanets.map((planet) => (
+        <div className="card" key={planet.name}>
+          <h2> {planet.name}</h2>
+          <img src={planet.image} alt="" />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default PlanetCard;
--- server/models/PlanetModel.js
+++ server/models/PlanetModel.js
@@ -16,12 +16,45 @@ const planetSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
+  type: {
+    type: String,
+    required: true,
+  },
+  radius: {
+    type: Number,
+    required: true,
+  },
+  yearLenght: {
+    type: Number,
+    required: true,
+  },
+  dayLenght: {
+    type: Number,
+    required: true,
+  },
+  distanceFromSun: {
+    type: Number,
+    required: true,
+  },
+  lightFromSunTime: {
+    type: Number,
+    required: true,
+  },
+  lowTemperature: {
+    type: Number,
+    required: true,
+  },
+  highTemperature: {
+    type: Number,
+    required: true,
+  },
   description: {
     type: String,
     required: true,
   },
   image: {
     type: String,
+    required: true,
   },
 });
 
