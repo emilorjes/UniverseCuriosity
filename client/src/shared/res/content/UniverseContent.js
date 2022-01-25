7edb59f63180c226420d4be050ccb5a30290d1ed
Emil Örjes <emil.orjes@hotmail.com>
2022-01-25 11:59:40 +0100

    Add content folder with UniversContent.js file with content with content strings about universe

---
+++ client/src/shared/res/content/UniverseContent.js
@@ -0,0 +1,49 @@
+const universeShortDescribe = `The Universe is practically everything. From space, time, and their contents, which include planets, stars, galaxies, to all other forms of matter, and energy. Everything is part of the universe, and the universe is part of everything.`;
+
+const universeAge = `We now know that the universe is 13,800,000,000 years old, that’s 13.8 billion. That is a very long time.`;
+
+const bigBangShortDescribe = `The big bang is how astronomers explain the way the universe began. It is the idea that the universe began as just a single point, then expanded and stretched to grow as large as it is right now—and it is still stretching!`;
+
+const bigBangLongDescribe = `When the universe began, it was just hot, tiny particles mixed with light and energy. It was nothing like what we see now. As everything expanded and took up more space, it cooled down.
+
+    The tiny particles grouped together. They formed atoms. Then those atoms grouped together. Over lots of time, atoms came together to form stars and galaxies.
+    
+    The first stars created bigger atoms and groups of atoms. That led to more stars being born. At the same time, galaxies were crashing and grouping together. As new stars were being born and dying, then things like asteroids, comets, planets, and black holes formed!`;
+
+const galaxayShortDescribe = `A galaxy is a huge collection of gas, dust, and billions of stars and their solar systems, all held together by gravity.`;
+
+const galaxyLongDescribe = `We live on a planet called Earth that is part of our solar system. But where is our solar system? It’s a small part of the Milky Way Galaxy.
+
+A galaxy is a huge collection of gas, dust, and  of stars and their solar systems. A galaxy is held together by gravity. Our galaxy, the Milky Way, also has a  in the middle.
+
+
+a diagram that shows that a planet is part of a solar system, and the solar system is part of a galaxy
+
+When you look up at stars in the night sky, you’re seeing other stars in the Milky Way. If it’s really dark, far away from lights from cities and houses, you can even see the dusty bands of the Milky Way stretch across the sky.
+
+
+a photo of the Milky Way seen from a dark desert. The dusty colors light up the sky.
+The Milky Way Galaxy stretches across the sky at the Trona Pinnacles National Landmark in California. Photo by Ian Norman.
+
+
+There are many galaxies besides ours, though. There are so many, we can’t even count them all yet! The  looked at a small patch of space for 12 days and found 10,000 galaxies, of all sizes, shapes, and colors. Some scientists think there could be as many as  galaxies in the universe.
+
+
+a photo taken by the Hubble Space Telescope of thousands of small galaxies of all different shapes and colors.
+This is the picture taken by the Hubble Space Telescope showing thousands of galaxies. Even the tiny dots are whole galaxies. The universe is a very big place!
+
+
+Some galaxies are spiral-shaped like ours. They have curved arms that make it look like a pinwheel. Other galaxies are smooth and oval shaped. They’re called elliptical galaxies. And there are also galaxies that aren’t spirals or ovals. They have irregular shapes and look like blobs. The light that we see from each of these galaxies comes from the stars inside it.
+
+
+A diagram of the different shapes of galaxies: spiral, elliptical, and irregular.
+
+Sometimes galaxies get too close and smash into each other. Our Milky Way galaxy will someday bump into Andromeda, our closest galactic neighbor. But don’t worry. It won’t happen for about five billion years. But even if it happened tomorrow, you might not notice. Galaxies are so big and spread out at the ends that even though galaxies bump into each other, the planets and solar systems often don’t get close to colliding.`;
+
+export {
+  universeShortDescribe,
+  universeAge,
+  bigBangShortDescribe,
+  bigBangLongDescribe,
+  galaxayShortDescribe,
+};
