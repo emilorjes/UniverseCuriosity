18d50b21c5a4f060319557a9a6471ce7cbfdda50
Emil Örjes <74154767+emilorjes@users.noreply.github.com>
2022-01-26 15:15:13 +0100

    Add basic about content and change expoer in UniversetextContent. (#28)

---
+++ client/src/shared/res/content/AboutTextContent.js
@@ -0,0 +1,5 @@
+const about = `Universe Curiosity is a place where you can learn more about the Universe in a fun and easy way. Explore our Solar System and other parts of the Universe.`;
+
+const info = `All facts and info that is displayed on Universe Curiosity is from the official NASA website.`;
+
+export default { about, info };
