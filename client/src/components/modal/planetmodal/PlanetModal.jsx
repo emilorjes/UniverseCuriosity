f037b1327deceb4eb0f4ee2663e144d0492d8f8c
Emil Örjes <emil.orjes@hotmail.com>
2022-01-31 12:09:51 +0100

    Change modal css and position

--- client/src/components/modal/planetmodal/PlanetModal.jsx
+++ client/src/components/modal/planetmodal/PlanetModal.jsx
@@ -17,30 +17,32 @@ function PlanetModal({ planet, modalOpen }) {
         >
           X
         </button>
-        <h2>{planet.name}</h2>
-        <h4>{planet.description}</h4>
-        <h4>Position from sun: {planet.position}</h4>
-        <h4>Number of moons: {planet.moons}</h4>
-        <h4>
-          {planet.name} is of type: {planet.type}
-        </h4>
-        <h4>
-          The radius of {planet.name}: {planet.radius} km
-        </h4>
-        <h4>
-          Number of earth days to complete one revolution around the sun:{' '}
-          {planet.yearLenght}
-        </h4>
-        <h4>
-          The lenght of a day on {planet.name}: {planet.dayLenght} hrs
-        </h4>
-        <h4>Distance from sun: {planet.distanceFromSun} km</h4>
-        <h4>
-          Time for light to travel from sun to {planet.name}:{' '}
-          {planet.lightFromSunTime} sec
-        </h4>
-        <h4>Lowest surface temperature: - {planet.lowTemperature} °C</h4>
-        <h4>Highest surface temperature: {planet.highTemperature} °C</h4>
+        <div className="modal-content">
+          <h2>{planet.name}</h2>
+          <h4>{planet.description}</h4>
+          <h4>Position from sun: {planet.position}</h4>
+          <h4>Number of moons: {planet.moons}</h4>
+          <h4>
+            {planet.name} is of type: {planet.type}
+          </h4>
+          <h4>
+            The radius of {planet.name}: {planet.radius} km
+          </h4>
+          <h4>
+            Number of earth days to complete one revolution around the sun:{' '}
+            {planet.yearLenght}
+          </h4>
+          <h4>
+            The lenght of a day on {planet.name}: {planet.dayLenght} hrs
+          </h4>
+          <h4>Distance from sun: {planet.distanceFromSun} km</h4>
+          <h4>
+            Time for light to travel from sun to {planet.name}:{' '}
+            {planet.lightFromSunTime} sec
+          </h4>
+          <h4>Lowest surface temperature: - {planet.lowTemperature} °C</h4>
+          <h4>Highest surface temperature: {planet.highTemperature} °C</h4>
+        </div>
       </div>
     </div>
   );
